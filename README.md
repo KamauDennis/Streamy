953e855e65319a88b31452573809b2150adfbf59
KamauDennis <kamaudenise@gmail.com>
2022-11-02 21:43:53 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Streamy
+
+Streamy is a program that captures a stream of images from a camera in real time and records them to either a still image file or a video file.
+
+## Requirements
+
+Make sure python3 is installed in your system then run the following command to install opencv library.
+
+`pip install opencv-python`
+
+# Usage
+
+Use the following command to run the program:
+
+`python streamy.py`
+
+- *Spacebar* causes a screenshot to be taken
+- *Tab* causes a screencast(a video recording) to start/stop
+- *Esc* causes the application to quit
---
+++ streamy.py
@@ -0,0 +1,40 @@
+import cv2
+from managers import WindowManager,CaptureManager
+
+class streamy(object):
+  def __init__(self):
+    self._windowManager = WindowManager('Streamy',self.onKeypress)
+    self._captureManager = CaptureManager(
+      cv2.VideoCapture(0),self._windowManager,True)
+
+  def run(self):
+    """Run the main loop."""
+    self._windowManager.createWindow()
+    while self._windowManager.isWindowCreated:
+      self._captureManager.enterFrame()
+      #self._captureManager.frame()
+      frame = self._captureManager.frame
+      #print(frame)
+      self._captureManager.exitFrame()
+      self._windowManager.processEvents()
+
+  def onKeypress(self,keycode):
+    """Handle a keypress
+    
+    space  -> Take a screenshot.
+    tab    -> Start/stop recording  screencast.
+    escape -> Quit
+
+    """
+    if keycode == 32: #space
+      self._captureManager.writeImage('screenshot.png')
+    elif keycode == 9: #tab
+      if not self._captureManager.isWritingVideo:
+        self._captureManager.startWritingVideo('screencast.avi')
+      else:
+        self._captureManager.stopWritingVideo()
+    elif keycode == 27: # escape
+      self._windowManager.destroyWindow()
+
+if __name__ == "__main__":
+  streamy().run()          
